06da2bb1f25a022f60338f1df2da52480835c2dd
Werner Janse van Rensburg <wernerjvanrensburg15@gmail.com>
2024-02-20 13:36:18 +0200

    Create method to get input from buttons and put together as a string

--- index.js
+++ index.js
@@ -1,11 +1,65 @@
 console.log("Js is connected")
 
-const numOne = document.querySelector('#numOne');
-const textBox = document.querySelector('#show')
+const butOne = document.querySelector('#numOne');
+const butNumOne = document.querySelector('#butNOne');
+const butNumTwo = document.querySelector('#butNTwo')
+const operatorOne = document.querySelector("#divide");
+const operatorTwo = document.querySelector('#btnMultiply')
+const answerBtn = document.querySelector('#equalBtn')
+const textBox = document.querySelector('#show');
+const clear = document.querySelector('#clear');
+let answerString = " ";
 
 
-function getInput() {
-    textBox.innerHTML = "Hello"
+
+function getString(buttonValue) {
+    
+    answerString += buttonValue;
+    console.log(answerString);  
 }
 
-numOne.addEventListener('click', getInput);
+function getAnswer() {
+    let test = answerString.split('x' || '+')
+    console.log(test);
+}
+
+function getAction() {
+
+}
+
+
+function clearInput() {
+    let stringArr = [];
+    for(let i = 0; i < answerString.length; i++) {
+        stringArr.push(answerString[i]);
+    }
+    console.log(answerString);
+    answerString = answerString.slice(0, -1);
+    let stringWithoutCommas = answerString.replace(/,/g, '');
+    console.log(stringWithoutCommas);
+}
+
+
+
+butOne.addEventListener('click', () => {
+    getString(butOne.value);
+});
+
+operatorOne.addEventListener('click', () => {
+    getString(operatorOne.value);
+})
+
+operatorTwo.addEventListener('click', () => {
+    getString(operatorTwo.value);
+} )
+
+butNumOne.addEventListener('click', () => {
+    getString(butNumOne.value);
+})
+
+butNumTwo.addEventListener('click', () => {
+    getString(butNumTwo.value);
+} )
+
+answerBtn.addEventListener('click', getAnswer);
+clear.addEventListener('click', clearInput);
